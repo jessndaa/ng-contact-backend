258fd4b78eac8e2ad62a3209c431d9308e380f4b
Jessy Ndaya <jessyndaya15@gmail.com>
2021-06-13 14:27:54 +0100

    fix https certificate on 5001 port

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import * as swaggerDocument from './docs/swagger.json';
 import cors from 'cors'; 
 import { router } from './routes/index';
 import * as path from 'path';
+import * as https  from 'https';
+import * as fs  from 'fs';
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -24,7 +26,11 @@ app.use(express.static(path.join(__dirname, 'public'), {}))
 app.use(cors()); 
 app.use('/api',  router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+https.createServer({
+    key: fs.readFileSync(path.resolve(__dirname+'/certs/server/local.key')),
+    cert: fs.readFileSync(path.resolve(__dirname+ '/certs/server/local.cert'))
+}, app)
+.listen(5001); 
 app.listen(port, ()=>{
     console.log("The serve is runing" + port);
 })
